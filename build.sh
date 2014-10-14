f688e636477e68b635d6a0ba9d8c3f63be306bbc
Gordon Fontenot <gordon@thoughtbot.com>
2014-10-14 16:30:16 -0400

    tweakin

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
+echo "Building frameworks"
 xcodebuild -project Liftoff.xcodeproj \
   -scheme liftoff \
   -derivedDataPath build \
   LD_RUNPATH_SEARCH_PATHS="/Users/gordon/Code/Frameworks"
+echo "done building"
