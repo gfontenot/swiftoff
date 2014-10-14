b795c0da05b3006b1c96634b9fe32f2a1036beb0
Gordon Fontenot <gordon@thoughtbot.com>
2014-10-14 16:48:26 -0400

    Add frameworks

--- build.sh
+++ build.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
-
-echo "Building frameworks"
 xcodebuild -project Liftoff.xcodeproj \
   -scheme liftoff \
   -derivedDataPath build \
   LD_RUNPATH_SEARCH_PATHS="__LIB__"
-echo "done building"
+
+mv build/Build/Products/Debug/*.framework frameworks
